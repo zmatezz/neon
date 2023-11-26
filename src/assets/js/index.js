bdc2d252076c51ce08938a900f1858eadd2e197a
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-11-26 14:12:36 -0300

    fix: responsive digital account

---
+++ src/assets/js/index.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".depoimentos", {
+  slidesPerView: 3,
+  spaceBetween: 32,
+  pagination: {
+    el: ".paginator-depoimentos",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    100: {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 1.9,
+      spaceBetween: 15,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
